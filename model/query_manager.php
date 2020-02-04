e5f80ae2e8d8704dae3e8145a513a8c4984b1ff9
William Pommies <williampommies@MacBook-Pro-de-William.local>
2020-02-04 19:53:46 +0100

    updated db connection files

---
+++ model/query_manager.php
@@ -0,0 +1,21 @@
+<?php
+require_once('model/database.php');
+
+class QueryManager
+{
+  public function __construct()
+  {
+    $filename = "config/dbconfig.json";
+    $database = new Database($filename);
+    $this->username = $database->getUsername();
+    $this->host = $database->getHost();
+    $this->password = $database->getPassword();
+    $this->dbname = $database->getDbname();
+  }
+
+  public function dbConnect()
+  {
+    $db = new PDO('mysql:host='.$this->host.';dbname='.$this->dbname.';charset=utf8', $this->username, $this->password);
+    return $db;
+  }
+}
