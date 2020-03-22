2eaa03be3ffa7fe0afaf6b7464f3d8082b6119db
William Pommies <williampommies@MacBook-Pro-de-William.local>
2020-03-22 15:07:12 +0100

    corrected spelling mistake in title

--- view/templates/header.php
+++ view/templates/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width-device-width, initial-scale=1.0">
-    <title>Billets Simple Pour l'Alaska</title>
+    <title>Billet Simple Pour l'Alaska</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="icon" type="image/png" href="./public/images/icon.png">
     <link rel="stylesheet" href="/public/style.css">
